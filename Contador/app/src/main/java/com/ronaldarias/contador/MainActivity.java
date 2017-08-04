7cb24dd3a025d7ff478b1597193a70e85b295669
rparias <ron_1192@hotmail.com>
2017-08-03 23:18:23 -0500

    Validar CheckBox y TryCatch

--- Contador/app/src/main/java/com/ronaldarias/contador/MainActivity.java
+++ Contador/app/src/main/java/com/ronaldarias/contador/MainActivity.java
@@ -3,17 +3,22 @@ package com.ronaldarias.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     //variable para contar las pulsaciones
     public int contador;
+    TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoResultado = (TextView)findViewById(R.id.txtContador);
 
         contador = 0;
     }
@@ -21,25 +26,40 @@ public class MainActivity extends Activity {
     //metodo para incrementar
     public void incrementarContador(View Vista){
         contador++;
-        mostrarResultado();
+        textoResultado.setText("" + contador);
     }
 
     //metodo para decrementar
     public void decrementarContador(View Vista){
         contador--;
-        mostrarResultado();
+        if(contador < 0){
+            CheckBox negativos = (CheckBox)findViewById(R.id.negativos);
+            if(!negativos.isChecked())  //si no esta activado
+                contador = 0;
+        }
+        textoResultado.setText("" + contador);
     }
 
     //metodo para resetear
     public void resetContador(View Vista){
-        contador = 0;
-        mostrarResultado();
+        EditText numero_reset = (EditText)findViewById(R.id.txtReseteo);
+
+        //en caso de q no se introduzca un valor salta la excepcion
+        try{
+            contador = Integer.parseInt(numero_reset.getText().toString()); //toString() porq no devuelve un String
+        }catch (Exception e){
+            contador = 0;
+        }
+
+        numero_reset.setText("");
+        textoResultado.setText("" + contador);
     }
 
+    /*
     //metodo para mostrar el resultado del contador
     public void mostrarResultado(){
         //R.id.txtContador es la ruta en recursos del txt del contador
         TextView textoResultado = (TextView)findViewById(R.id.txtContador);
         textoResultado.setText(""+contador);
-    }
+    }*/
 }
