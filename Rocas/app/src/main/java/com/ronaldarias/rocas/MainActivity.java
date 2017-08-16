1a76cb107a35a1ca15c5039a6d618190a0c3fdc2
rparias <ron_1192@hotmail.com>
2017-08-15 21:59:28 -0500

    ToolBar

--- Rocas/app/src/main/java/com/ronaldarias/rocas/MainActivity.java
+++ Rocas/app/src/main/java/com/ronaldarias/rocas/MainActivity.java
@@ -3,16 +3,24 @@ package com.ronaldarias.rocas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //registro el nuevo toolbar
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        //activo el toolbar
+        setSupportActionBar(toolbar);
     }
 
     public void ejecutar_info(View view){
